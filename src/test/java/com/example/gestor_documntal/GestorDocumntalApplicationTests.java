9aedfebc8fab300cf80ea85de538afb77cd33a34
SamNegDev <sam.negrin@gmail.com>
2026-03-13 18:51:40 +0000

    Initial commit - project setup and dependencies

---
+++ src/test/java/com/example/gestor_documntal/GestorDocumntalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestor_documntal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDocumntalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
